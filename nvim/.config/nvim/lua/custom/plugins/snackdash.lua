9593e51d6e5c46266ea8b9bf9df4e058a5e50d5d
Lucas Deichsel <luc_dei@uni-bremen.de>
2025-08-10 01:37:42 +0200

    update snacksdash

--- nvim/.config/nvim/lua/custom/plugins/snackdash.lua
+++ nvim/.config/nvim/lua/custom/plugins/snackdash.lua
@@ -8,41 +8,6 @@ return {
       enabled = true,
       width = 80,
       preset = {
-        -- Custom header - replace this with your desired ASCII art or text
-        --        header = [[
-        --            :h-                                  Nhy`
-        --           -mh.                           h.    `Ndho
-        --           hmh+                          oNm.   oNdhh
-        --          `Nmhd`                        /NNmd  /NNhhd
-        --          -NNhhy                      `hMNmmm`+NNdhhh
-        --          .NNmhhs              ```....`..-:/./mNdhhh+
-        --           mNNdhhh-     `.-::///+++////++//:--.`-/sd`
-        --           oNNNdhhdo..://++//++++++/+++//++///++/-.`
-        --      y.   `mNNNmhhhdy+/++++//+/////++//+++///++////-` `/oos:
-        -- .    Nmy:  :NNNNmhhhhdy+/++/+++///:.....--:////+++///:.`:s+
-        -- h-   dNmNmy oNNNNNdhhhhy:/+/+++/-         ---:/+++//++//.`
-        -- hd+` -NNNy`./dNNNNNhhhh+-://///    -+oo:`  ::-:+////++///:`
-        -- /Nmhs+oss-:++/dNNNmhho:--::///    /mmmmmo  ../-///++///////.
-        --  oNNdhhhhhhhs//osso/:---:::///    /yyyyso  ..o+-//////////:/.
-        --   /mNNNmdhhhh/://+///::://////     -:::- ..+sy+:////////::/:/.
-        --     /hNNNdhhs--:/+++////++/////.      ..-/yhhs-/////////::/::/`
-        --       .ooo+/-::::/+///////++++//-/ossyyhhhhs/:///////:::/::::/:
-        --       -///:::::::////++///+++/////:/+ooo+/::///////.::://::---+`
-        --       /////+//++++/////+////-..//////////::-:::--`.:///:---:::/:
-        --       //+++//++++++////+++///::--                 .::::-------::
-        --       :/++++///////////++++//////.                -:/:----::../-
-        --       -/++++//++///+//////////////               .::::---:::-.+`
-        --       `////////////////////////////:.            --::-----...-/
-        --        -///://////////////////////::::-..      :-:-:-..-::.`.+`
-        --         :/://///:///::://::://::::::/:::::::-:---::-.-....``/- -
-        --           ::::://::://::::::::::::::----------..-:....`.../- -+oo/
-        --            -/:::-:::::---://:-::-::::----::---.-.......`-/.      ``
-        --           s-`::--:::------:////----:---.-:::...-.....`./:
-        --          yMNy.`::-.--::..-dmmhhhs-..-.-.......`.....-/:`
-        --         oMNNNh. `-::--...:NNNdhhh/.--.`..``.......:/-
-        --        :dy+:`      .-::-..NNNhhd+``..`...````.-::-`
-        --                        .-:mNdhh:.......--::::-`
-        --                           yNh/..------..`                          ]],
         header = [[                                                 
                                                                   
      ████ ██████           █████      ██                    
@@ -55,32 +20,31 @@ return {
       },
       sections = {
         { section = 'header' },
+        {
+          pane = 2,
+          section = 'terminal',
+          cmd = 'astroterm --color --constellations --speed 10000 --fps 64 --city Singapore',
+          height = 35,
+        },
+        { section = 'keys', gap = 1, padding = 1 },
         -- {
         --   pane = 2,
-        --   section = 'terminal',
-        --   cmd = 'powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\\Users\\ludei\\scripts\\colorblocks.ps1"',
-        --   height = 5,
+        --   icon = ' ',
+        --   title = 'Recent Files',
+        --   section = 'recent_files',
+        --   indent = 2,
         --   padding = 1,
         -- },
-        { section = 'keys', gap = 1, padding = 1 },
+        -- {
+        --   pane = 2,
+        --   icon = ' ',
+        --   title = 'Projects',
+        --   section = 'projects',
+        --   indent = 2,
+        --   padding = 1,
+        -- },
         {
-          pane = 2,
-          icon = ' ',
-          title = 'Recent Files',
-          section = 'recent_files',
-          indent = 2,
-          padding = 1,
-        },
-        {
-          pane = 2,
-          icon = ' ',
-          title = 'Projects',
-          section = 'projects',
-          indent = 2,
-          padding = 1,
-        },
-        {
-          pane = 2,
+          pane = 1,
           icon = ' ',
           title = 'Git Status',
           section = 'terminal',
