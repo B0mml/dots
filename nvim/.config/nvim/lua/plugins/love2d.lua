438a7d2b7ec838b1b5b6a931a9707e4f50e42cfc
Lucas Deichsel <luc_dei@uni-bremen.de>
2025-08-10 04:03:39 +0200

    add keybind for run with console

--- nvim/.config/nvim/lua/plugins/love2d.lua
+++ nvim/.config/nvim/lua/plugins/love2d.lua
@@ -1,17 +1,21 @@
--- LÖVE 2D game development plugin
 return {
   {
     'S1M0N38/love2d.nvim',
     event = 'VeryLazy',
     version = '2.*',
-    opts = {
-      debug_window_opts = {
-        split = 'right',
-      },
-    },
+    opts = {},
     keys = {
       { '<leader>v', ft = 'lua', desc = 'LÖVE' },
       { '<leader>vv', '<cmd>LoveRun<cr>', ft = 'lua', desc = 'Run LÖVE' },
+      {
+        '<leader>vc',
+        function()
+          require('love2d').setup { debug_window_opts = { split = 'right' } }
+          vim.cmd 'LoveRun'
+        end,
+        ft = 'lua',
+        desc = 'Run LÖVE with console',
+      },
       { '<leader>vs', '<cmd>LoveStop<cr>', ft = 'lua', desc = 'Stop LÖVE' },
     },
   },
