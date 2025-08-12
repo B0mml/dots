d02ca46042c28a4e0c23c01a31c808f0dd63e311
Lucas Deichsel <luc_dei@uni-bremen.de>
2025-08-13 00:57:00 +0200

    update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,17 @@ if not set -q TMUX; and not tmux has-session -t default 2>/dev/null
     tmux -2u new -s default
 end
 
+
+alias rm='rm -I'
+alias mv='mv -i'
+alias cp='cp -i'
+alias un='paru -Rns' # uninstall package
+alias up='paru -Syu' # update system/package/aur
+alias pl='paru -Qs' # list installed package
+alias pa='paru -Ss' # list availabe package
+alias pc='paru -Sc' # remove unused cache
+alias po='paru -Qtdq | paru -Rns -' # remove unused packages, also try > paru -Qqd | paru -Rsu --print -
+
 alias cd='z'
 alias n='nvim'
 alias lg='lazygit'
@@ -14,6 +25,41 @@ alias lowfi="lowfi -t chillhop"
 
 function todoo
     todo q "$argv #Inbox"
+end
+
+function ex
+    if test -f $argv[1]
+        switch $argv[1]
+            case "*.tar.bz2"
+                tar xjf $argv[1]
+            case "*.tar.gz"
+                tar xzf $argv[1]
+            case "*.tar.xz"
+                tar xJf $argv[1]
+            case "*.bz2"
+                bunzip2 $argv[1]
+            case "*.rar"
+                unrar x $argv[1]
+            case "*.gz"
+                gunzip $argv[1]
+            case "*.tar"
+                tar xf $argv[1]
+            case "*.tbz2"
+                tar xjf $argv[1]
+            case "*.tgz"
+                tar xzf $argv[1]
+            case "*.zip"
+                unzip $argv[1]
+            case "*.Z"
+                uncompress $argv[1]
+            case "*.7z"
+                7z x $argv[1]
+            case "*"
+                echo "'$argv[1]' cannot be extracted via ex()"
+        end
+    else
+        echo "'$argv[1]' is not a valid file"
+    end
 end
 
 zoxide init fish | source
