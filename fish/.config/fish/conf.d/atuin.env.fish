1c04becac6ae7969860534a988d75451c5715c1e
Lucas Deichsel <luc_dei@uni-bremen.de>
2026-01-08 02:15:51 +0100

    update fish and ghostty

--- fish/.config/fish/conf.d/atuin.env.fish
+++ fish/.config/fish/conf.d/atuin.env.fish
@@ -1,2 +1,6 @@
 source "$HOME/.atuin/bin/env.fish"
-atuin init fish | source
+# atuin init fish | source
+# Workaround für Fish 4.0 und Atuin (entfernt das veraltete -k Flag)
+if status is-interactive
+    atuin init fish | sed 's/-k up/up/' | source
+end
